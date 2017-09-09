191b14fe636c7cc1e13d104a85c31bce0504fad0
Zenyr <zenyr@zenyr.com>
2017-09-09 15:54:03 +0900

    add execute2 alternative method

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ type styledString = styledChar[];
 */
 
 const execute = styledString => {
-  // simple for loop, only for the base spec
+  // [Method 1] simple for loop, only for the base spec
   const aChars = [];
   if (styledString && styledString.length) {
     let wasBold = false;
@@ -56,11 +56,88 @@ const execute = styledString => {
 
   return `<p>${aChars.join('')}</p>`;
 };
+
+
+const SHOW_TREE = false;
+const execute2 = styledString => {
+  // [Method 2] for demo purpose, pseudo AST
+  let i = 0;
+  const parse = (tagName, currentStyle = {}) => {
+    const oResult = {
+      tagName,
+      children: [],
+    };
+    if (!styledString || !styledString.length) return oResult;
+    let lastChar = '';
+    while (i < styledString.length) {
+      const { char, style } = styledString[i];
+      const { BOLD, ITALIC } = style;
+      let parsed = false;
+      if (currentStyle.BOLD) {
+        if (!BOLD) {
+          break;
+        }
+      } else {
+        if (BOLD) {
+          oResult.children.push(
+            parse('b', Object.assign({}, currentStyle, { BOLD }))
+          );
+          parsed = true;
+        }
+      }
+      if (currentStyle.ITALIC) {
+        if (!ITALIC) {
+          break;
+        }
+      } else {
+        if (ITALIC) {
+          oResult.children.push(
+            parse('i', Object.assign({}, currentStyle, { ITALIC }))
+          );
+          parsed = true;
+        }
+      }
+      if (!parsed) {
+        if (typeof oResult.children[oResult.children.length - 1] === 'string') {
+          oResult.children[oResult.children.length - 1] +=
+            char === ' ' && lastChar === ' ' ? '&nbsp;' : char;
+          lastChar = char;
+        } else {
+          oResult.children.push(char);
+          lastChar = char;
+        }
+        i++;
+      }
+    }
+    return oResult;
+  };
+
+  const read = oNode => {
+    if (typeof oNode === 'string') return oNode;
+    const { tagName, str, children } = oNode;
+    if (tagName !== 'p' && children.length === 0) return '';
+    return `<${tagName}>${children
+      .map(oInnerNode => read(oInnerNode))
+      .join('')}</${tagName}>`;
+  };
+
+  const oRoot = parse('p');
+  const result = read(oRoot);
+  // unroll and return
+  if (SHOW_TREE) {
+    console.log(JSON.stringify(oRoot, null, 1));
+  }
+
+  // return execute(styledString);
+  return result;
+};
+
 /*
 @param styledString: an array of nested js objects that contains information about characters and their styles
 @return xmlString: string;
 */
 module.exports = function(styledString) {
   // WARNING: Do not use string.replace or regex operations.
-  return execute(styledString);
+  // return execute(styledString);
+  return execute2(styledString);
 };
