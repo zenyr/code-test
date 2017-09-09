a4cf9bb167b88e97d0accbd6d5550b4b17480330
Zenyr <zenyr@zenyr.com>
2017-09-09 14:55:40 +0900

    implement execute code

--- src/index.js
+++ src/index.js
@@ -24,11 +24,39 @@ interface styledChar {
 type styledString = styledChar[];
 */
 
+const execute = styledString => {
+  // simple for loop, only for the base spec
+  const aChars = [];
+  if (styledString && styledString.length) {
+    let wasBold = false;
+    let wasItalic = false;
+    for (let i = 0; i < styledString.length; i++) {
+      const { char, style } = styledString[i];
+      const { BOLD: bold, ITALIC: italic } = style;
+      // 1. Should I close tags?
+      if (wasItalic && !italic) aChars.push('</i>');
+      if (wasBold && !bold) aChars.push('</b>');
+      // 2. Should I open tags?
+      if (!wasBold && bold) aChars.push('<b>');
+      if (!wasItalic && italic) aChars.push('<i>');
+      // 3. put a char
+      aChars.push(char);
+      // 4. save state for next/end of the loop
+      wasBold = bold;
+      wasItalic = italic;
+    }
+    // 4. Should I close tags?
+    if (wasItalic) aChars.push('</i>');
+    if (wasBold) aChars.push('</b>');
+  }
+
+  return `<p>${aChars.join('')}</p>`;
+};
 /*
 @param styledString: an array of nested js objects that contains information about characters and their styles
 @return xmlString: string;
 */
 module.exports = function(styledString) {
   // WARNING: Do not use string.replace or regex operations.
-  return '<p></p>'; // FILL ME
-}
+  return execute(styledString);
+};
